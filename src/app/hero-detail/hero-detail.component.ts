f04d990af81e0d4862380103a69a8067bbc3497f
vuong <nmvuong92@gmail.com>
2018-01-09 01:42:40 +0700

    initial

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero }         from '../hero';
+import { HeroService }  from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
+})
+export class HeroDetailComponent implements OnInit {
+  //lấy tham số [hero] truyền vào khi gọi components này từ template
+  //<app-hero-detail [hero]="selectedHero"></app-hero-detail>
+  @Input() hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    //lấy tham số ID parameter route
+    const id = +this.route.snapshot.paramMap.get('id');
+    //lấy danh sách  hero
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
